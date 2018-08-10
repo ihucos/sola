2f0740dc2d44c917161b524a1d92761f383bd106
Irae Hueck Costa <irae@irae.me>
2018-08-10 20:56:25 +0200

    Changed sliders a little bit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sola',
-    version='1.0',
+    version='1.1',
     description='Customize the solarized theme for your terminal',
     url='https://github.com/ihucos/sola',
     scripts=['sola'],
